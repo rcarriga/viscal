9e04af9506f5f44bcd7aa249f91a277c497c23ba
Rónán Carrigan <rcarriga@tcd.ie>
2020-03-02 12:09:58 +0000

    Add output to application shape

--- src/board/content/elements/application.tsx
+++ src/board/content/elements/application.tsx
@@ -14,6 +14,7 @@ const Appl = (props: ExprProps) => {
       id={props.id}
       events={events}
       height={coord.h}
+      heightMargin={dimensions.heightMargin}
       radius={dimensions.circleRadius}
       rest={props.rest}
       start={props.start}
@@ -29,21 +30,30 @@ export default Appl
 
 interface RawApplProps extends RawExprProps {
   radius: number
+  heightMargin: number
   width: number
   height: number
   style: ApplStyle
 }
 
 const RawAppl = (props: RawApplProps) => {
-  const boxWidth = props.width
-  const offsetWidth = props.height / 2
-  const boxPath = `M${props.x},${props.y}
-          l${offsetWidth},${props.height / 2}
-          l${boxWidth - offsetWidth * 2},0
-          l${offsetWidth},${-props.height / 2}
-          l${-offsetWidth},${-props.height / 2}
-          l${offsetWidth * 2 - boxWidth},0
-          l${-offsetWidth},${props.height / 2}`
+  const boxWidth = props.width - props.radius
+  const bufferOffset = 1
+  const circleBuffer = props.height / 2 - props.radius - bufferOffset
+  const outPath = `M${props.x + props.radius},${props.y - props.radius}
+        a1,1 0 0,0 0,${props.radius * 2}`
+  const boxPath = `M${props.x + props.radius},${props.y + props.radius}
+        l0,${bufferOffset}
+        l${props.heightMargin},${circleBuffer}
+        l${boxWidth - props.heightMargin * 2},0
+        l${props.heightMargin},${-circleBuffer}
+        l0,${-bufferOffset}
+        l0,${-props.radius * 2}
+        l0,${-bufferOffset}
+        l${-props.heightMargin},${-circleBuffer}
+        l${props.heightMargin * 2 - boxWidth},0
+        l${-props.heightMargin},${circleBuffer}
+        l0,${bufferOffset + props.radius * 2}`
 
   const boxAnimate = useMotion(
     {
@@ -54,15 +64,35 @@ const RawAppl = (props: RawApplProps) => {
     props.rest,
     props.start
   )
+  const outAnimate = useMotion(
+    {
+      d: outPath,
+      fill: props.style.output.fill,
+      ...props.style.output.stroke
+    },
+    props.rest,
+    props.start
+  )
 
   return (
-    <animated.path
-      {...boxAnimate}
-      strokeLinecap="round"
-      pointerEvents="stroke"
-      onClick={() => props.events.click(props.id)}
-      onMouseOver={() => props.events.highlight(props.id)}
-      onMouseLeave={() => props.events.clearHighlight(props.id)}
-    />
+    <g>
+      <animated.path
+        {...outAnimate}
+        onClick={e => {
+          e.stopPropagation()
+          props.events.click(props.id)
+        }}
+        onMouseOver={() => props.events.highlight(props.id)}
+        onMouseLeave={() => props.events.clearHighlight(props.id)}
+      />
+      <animated.path
+        {...boxAnimate}
+        strokeLinecap="round"
+        pointerEvents="stroke"
+        onClick={() => props.events.click(props.id)}
+        onMouseOver={() => props.events.highlight(props.id)}
+        onMouseLeave={() => props.events.clearHighlight(props.id)}
+      />
+    </g>
   )
 }
