9e2cae4c6ada834aae905bd2c4e44f6e65316fab
Rónán Carrigan <rcarriga@tcd.ie>
2020-03-03 15:10:20 +0000

    Made controls collapsible

--- src/board/control/index.tsx
+++ src/board/control/index.tsx
@@ -1,33 +1,52 @@
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown"
 import AnimationControl from "board/control/animation"
-import React from "react"
+import React, { useState } from "react"
+import { animated, useTransition, useSpring } from "react-spring"
 
 import ExpressionControl from "./expression"
 import ReducerControl from "./reducer"
 import ScaleControl from "./scale"
 
-const BoardControl = () => {
-  return (
-    <ControlPanel>
-      <ExpressionControl />
-      <hr className="dropdown-divider" />
-      <ScaleControl />
-      <hr className="dropdown-divider" />
-      <AnimationControl />
-      <hr className="dropdown-divider" />
-      <ReducerControl />
-      <hr className="dropdown-divider" />
-    </ControlPanel>
-  )
-}
-
-export default BoardControl
-
 const ControlPanel = (props: any) => {
+  const [show, setShow] = useState(true)
+  const arrow = useSpring({
+    display: "inline-block",
+    transform: show ? "rotate(180deg)" : "rotate(720deg)"
+  })
+  const children = useSpring({
+    display: "block",
+    padding: 5,
+    maxHeight: show ? 300 : 0,
+    opacity: show ? 1 : 0,
+    overflow: show ? "visible" : "hidden",
+    clear: "both"
+  })
+
   return (
-    <div className="card" style={{ width: 250 }}>
-      <div className="card-content">
-        <div className="menu">{props.children}</div>
+    <div className="box" style={{ width: 250 }}>
+      <div
+        style={{
+          cursor: "pointer"
+        }}
+        onClick={() => setShow(!show)}
+      >
+        <div className="subtitle" style={{ float: "left" }}>
+          Controls
+        </div>
+        <div style={{ float: "right" }}>
+          <animated.div style={arrow}>
+            <ArrowDropDownIcon />
+          </animated.div>
+        </div>
       </div>
+      <animated.div className="menu" style={children}>
+        <ExpressionControl />
+        <ScaleControl />
+        <AnimationControl />
+      </animated.div>
+      <ReducerControl />
     </div>
   )
 }
+
+export default ControlPanel
