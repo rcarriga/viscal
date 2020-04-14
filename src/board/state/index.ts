92bec908da39ca0a57d4d8370e7e57b1faa4df75
Rónán Carrigan <rcarriga@tcd.ie>
2020-04-14 11:29:21 +0100

    Group reduction stages with no effects for undo

--- src/board/state/index.ts
+++ src/board/state/index.ts
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit"
+import { TreeState } from "board/state/tree"
 import { useDispatch as _useDispatch } from "react-redux"
-import undoable, { includeAction } from "redux-undo"
+import undoable, { includeAction, StateWithHistory } from "redux-undo"
 import treeSlice from "./tree/reducers"
 import visualSlice from "./visual/reducers"
 export * from "./tree"
@@ -12,11 +13,23 @@ type TreeAction = keyof typeof treeSlice.actions
 
 const include = (actions: TreeAction[]) => includeAction(actions.map(action => `tree/${action}`))
 
+const groupFunction = (_: TreeAction, currentState: TreeState, stateHistory: StateWithHistory<TreeState>) => {
+  const reduction = currentState.reduction || stateHistory.present.reduction
+  if (reduction && Object.keys(reduction.substitutions).length === 0)
+    switch (reduction.type) {
+      case "FADE":
+      case "REMOVE":
+        return "NO_EFFECT"
+      default:
+    }
+  return null
+}
+
 const store = configureStore({
   reducer: {
     tree: undoable(treeSlice.reducer, {
-      ignoreInitialState: true,
-      filter: include(["setRoot", "queueReduction", "nextReductionStage"])
+      filter: include(["setRoot", "queueReduction", "nextReductionStage"]),
+      groupBy: groupFunction as any
     }),
     visual: visualSlice.reducer
   }
