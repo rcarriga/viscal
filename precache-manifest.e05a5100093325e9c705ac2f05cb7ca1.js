a323cd6a15e74c52b1ddd7d31e967db05dae7508
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-14 10:45:52 +0000

    Deploy rcarriga/viscal to github.com/rcarriga/viscal.git:gh-pages

---
+++ precache-manifest.e05a5100093325e9c705ac2f05cb7ca1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2f494ebe5f3c1c03c035cf594a017a",
+    "url": "/viscal/index.html"
+  },
+  {
+    "revision": "3cc28130b5fd4a5928d3",
+    "url": "/viscal/static/css/2.cf932dc2.chunk.css"
+  },
+  {
+    "revision": "beb3af010613a9f3f161",
+    "url": "/viscal/static/css/main.80889977.chunk.css"
+  },
+  {
+    "revision": "3cc28130b5fd4a5928d3",
+    "url": "/viscal/static/js/2.bf5aa143.chunk.js"
+  },
+  {
+    "revision": "aba147b67885c0da4d43060c14298cc5",
+    "url": "/viscal/static/js/2.bf5aa143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb3af010613a9f3f161",
+    "url": "/viscal/static/js/main.640ec07d.chunk.js"
+  },
+  {
+    "revision": "0fc5d940b0cca5d08eff",
+    "url": "/viscal/static/js/runtime-main.c535778d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/viscal/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/viscal/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/viscal/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/viscal/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/viscal/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/viscal/precache-manifest.e05a5100093325e9c705ac2f05cb7ca1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/viscal/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
